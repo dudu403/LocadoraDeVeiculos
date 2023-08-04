84147a624da0b439c64a74a65770753f968fb07a
Jesiane Straubel <jesi1951@hotmail.com>
2023-08-04 20:00:53 -0300

    .

--- LocadoraDeVeiculos.Dominio/ModuloPlanoCobranca/PlanoCobranca.cs
+++ LocadoraDeVeiculos.Dominio/ModuloPlanoCobranca/PlanoCobranca.cs
@@ -11,9 +11,20 @@ namespace LocadoraDeVeiculos.Dominio.ModuloPlanoCobranca
         public decimal ?precoPorKmExtrapolado { get; set; }
         public decimal ?kmDisponiveis { get; set; }
 
+        public PlanoCobranca()
+        {
+          
+        }
+
         public override void AtualizarInformacoes(PlanoCobranca registroAtualizado)
         {
-            throw new NotImplementedException();
+            grupoAutomovel = registroAtualizado.grupoAutomovel;
+            tipoPlano = registroAtualizado.tipoPlano;
+            precoDiaria = registroAtualizado.precoDiaria;
+            precoPorKm = registroAtualizado.precoPorKm;
+            precoPorKmExtrapolado = registroAtualizado.precoPorKmExtrapolado;
+            kmDisponiveis = registroAtualizado.kmDisponiveis;
+                
         }
     }
 }
